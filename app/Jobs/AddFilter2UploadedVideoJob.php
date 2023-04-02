3c4eb0ec46a48ac784f4908cd65e18e3e685b490
farzad foruozanfar <foruozanfar2000f@gmail.com>
2023-04-03 00:43:18 +0330

    add enable watermark field to CreateVideo api

--- app/Jobs/AddFilter2UploadedVideoJob.php
+++ app/Jobs/AddFilter2UploadedVideoJob.php
@@ -22,18 +22,21 @@ class AddFilter2UploadedVideoJob implements ShouldQueue
     private Video $video;
     private string|int $video_id;
     private $user;
+    private bool $enable_watermark;
 
     /**
      * Create a new job instance.
      * @param Video $video
      * @param $video_id
+     * @param $enable_watermark
      */
-    public function __construct(Video $video, $video_id)
+    public function __construct(Video $video, $video_id, bool $enable_watermark)
     {
 
         $this->video    = $video;
         $this->video_id = $video_id;
         $this->user     = auth()->user();
+        $this->enable_watermark = $enable_watermark;
     }
 
     /**
@@ -46,11 +49,15 @@ class AddFilter2UploadedVideoJob implements ShouldQueue
             $tmpPath       = '/tmp/' . $this->video_id;
             $channelName   = $this->user->channel->name;
             $videoFile     = FFMpeg::fromDisk('video')->open($tmpPath);
-            $filter        = new CustomFilter("drawtext=text='aparat.me/{$channelName}': fontcolor=blue: fontsize=24:
-            box=1: boxcolor=white@0.4: boxborderw=5:
-            x=10: y=(h - text_h - 10)");
-            $format      = new \FFMpeg\Format\Video\X264('libmp3lame');
-            $videoFilter = $videoFile->addFilter($filter)->export()->toDisk('video')->inFormat($format);
+            $format        = new \FFMpeg\Format\Video\X264('libmp3lame');
+            if ($this->enable_watermark)
+            {
+                $filter = new CustomFilter("drawtext=text='aparat.me/{$channelName}': fontcolor=blue: fontsize=24:
+                          box=1: boxcolor=white@0.4: boxborderw=5:
+                          x=10: y=(h - text_h - 10)");
+                $videoFile = $videoFile->addFilter($filter);
+            }
+            $videoFilter = $videoFile->export()->toDisk('video')->inFormat($format);
             $videoFilter->save($this->user->id . '/' . $this->video->slug . '.mp4');
             Storage::disk('video')->delete($tmpPath);
             $this->video->duration = $videoFile->getDurationInSeconds();
