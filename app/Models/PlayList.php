e80cd0210e3ee2c1a092804872d4e89f6a00f191
farzad foruozanfar <foruozanfar2000f@gmail.com>
2023-04-04 11:01:11 +0330

    add count(count of video in playlist)field

--- app/Models/PlayList.php
+++ app/Models/PlayList.php
@@ -9,9 +9,12 @@ class PlayList extends Model
 {
     use HasFactory;
 
+    //region model config
     protected $table   = 'playlist';
     protected $guarded = [];
+    //endregion model config
 
+    //region relations
     public function videos()
     {
         return $this->belongsToMany(Video::class, 'playlists_videos');
@@ -20,5 +23,14 @@ class PlayList extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    //endregion relations
+
+    public function toArray()
+    {
+        $data          = parent::toArray();
+        $data['count'] = $this->videos()->count();
+
+        return $data;
     }
 }
